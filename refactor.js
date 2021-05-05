adedf642082fbc997a930fdb6b055fcd44015f1e
katefranks <kmfranks9@gmail.com>
2021-05-04 21:36:39 -0400

    set timeout complete

--- refactor.js
+++ refactor.js
@@ -33,6 +33,9 @@ function doSomethingCool() {
 
 // Put your answer below -------------------------
 
+function() {
+  console.log("Something Cool!");
+}
 
 // -----------------------------------------------
 
@@ -53,6 +56,10 @@ setTimeout(sayHi, 2000);
 
 // Put your answer below -------------------------
 
+setTimeout(function () {
+  alert("Hello, World!");
+} setTimeout((), 2000);
+}
 
 // -----------------------------------------------
 
